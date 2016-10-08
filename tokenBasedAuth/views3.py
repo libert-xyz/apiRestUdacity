69869a540fd378632dfe9ca1c023a992bb2b5b70
Libert R Schmidt <rschmidt@nuvops.com>
2016-10-08 18:45:59 -0400

    bagels

---
+++ tokenBasedAuth/views3.py
@@ -0,0 +1,70 @@
+
+from models import Base, User
+from flask import Flask, jsonify, request, url_for, abort, g
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+
+engine = create_engine('sqlite:///usersWithTokens.db')
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+
+@auth.verify_password
+def verify_password(username_or_token,password):
+    #Try to see if it's a token first
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id = user_id).one()
+    else:
+        user = session.query(User).filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token':token.decode('ascii')})
+
+
+@app.route('/users', methods = ['POST'])
+def new_user():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    if username is None or password is None:
+        print "missing arguments"
+        abort(400)
+
+    if session.query(User).filter_by(username = username).first() is not None:
+        print "existing user"
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
+
+
+
+if '__main__' == __name__:
+    app.run()
+    app.debug = True
