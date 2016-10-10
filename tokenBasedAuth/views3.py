90477fc8a151c0ef243a3b2e4286ff4138ff9a72
Libert R Schmidt <rschmidt@nuvops.com>
2016-10-10 00:31:42 -0400

    oauth/myToken

--- tokenBasedAuth/views3.py
+++ tokenBasedAuth/views3.py
@@ -1,20 +1,34 @@
 
 from models import Base, User
-from flask import Flask, jsonify, request, url_for, abort, g
+from flask import Flask, jsonify, request, url_for, abort, g, render_template
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-
+import os
+import json
+import requests
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 
+
 engine = create_engine('sqlite:///usersWithTokens.db')
+
+#Google Imports
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+from flask import make_response
+
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
+
+CLIENT_ID = os.environ['google_client_id']
 
 @auth.verify_password
 def verify_password(username_or_token,password):
@@ -29,7 +43,84 @@ def verify_password(username_or_token,password):
     g.user = user
     return True
 
+@app.route('/clientOAuth')
+def start():
+    return render_template('clientOAuth.html')
 
+@app.route('/oauth/<provider>',methods=['POST'])
+def login(provider):
+    print '----------'
+    print 'login part'
+    #STEP1 - Parse the auth Code
+    #auth_code = request.json.get('auth_code')
+    #auth_code = request.args.get('auth_code')
+    auth_code = request.data
+
+    print 'Step 1 Complete, received auth code %s' %auth_code
+    if provider == 'google':
+        #STEP2 exchange for a token
+        try:
+            #Upgrade the auth code into a credentials object
+            oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+            oauth_flow.redirect_uri = 'postmessage'
+            credentials = oauth_flow.step2_exchange(auth_code)
+        except:
+            response = make_response(json.dumps('Failed to upgrade the authotization code'),401)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        #Check that the access token is valid
+        access_token = credentials.access_token
+        print 'access Token: %s' %access_token
+        url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+        h = httplib2.Http()
+        result = json.loads(h.request(url,'GET')[1])
+        # If there was an error in the access token info, abort.
+        if result.get('error') is not None:
+            response = make_response(json.dumps(result.get('error')), 500)
+            response.headers['Content-Type'] = 'application/json'
+
+        print "Step 2 Complete! Access Token : %s " % credentials.access_token
+
+        #STEP 3 - Find User or make a new one
+
+        h = httplib2.Http()
+        userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
+        params = {'access_token': credentials.access_token, 'alt':'json'}
+        answer = requests.get(userinfo_url, params=params)
+
+        data = answer.json()
+
+        name = data['name']
+        picture = data['picture']
+        email = data['email']
+
+        #see if user exists, if it doesn't make a new one
+
+        user = session.query(User).filter_by(email=email).first()
+        if not user:
+            user = User(username=name,picture=picture,email=email)
+            session.add(user)
+            session.commit()
+
+
+       #STEP 4 - Make our token
+        token = user.generate_auth_token(600)
+
+       #STEP 5 - Send back token to the client
+        print '---------'
+        print 'MY-APP Access Token %s' %token
+        return jsonify({'token':token.decode('ascii')})
+
+    else:
+       return 'Unrecoginized provider'
+
+@app.route('/show')
+def show_users():
+    users = session.query(User).all()
+    for i in users:
+        print i.email,i.id,i.password_hash
+    return 'ok'
 
 @app.route('/token')
 @auth.login_required
@@ -66,5 +157,5 @@ def get_resource():
 
 
 if '__main__' == __name__:
-    app.run()
     app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
